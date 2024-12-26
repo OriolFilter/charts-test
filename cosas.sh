af16ae21126f6efd2fba2b7cf54941e379014736
oriol <filter.oriol@gmail.com>
2024-12-26 21:14:43 +0100

    test

--- cosas.sh
+++ cosas.sh
@@ -1,5 +1,5 @@
-#helm dependency update prowlarr && \
-#helm install myprowlarr prowlarr/ --debug --dry-run
+helm dependency update prowlarr && \
+helm install myprowlarr prowlarr/ --debug --dry-run
 
-helm dependency update basetemplate && \
-helm install basetemplate basetemplate/ --debug --dry-run
+#helm dependency update basetemplate && \
+#helm install basetemplate basetemplate/ --debug --dry-run
