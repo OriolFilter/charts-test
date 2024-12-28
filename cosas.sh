86ddb5aded21c17868182a5d31720626331109a9
oriol <filter.oriol@gmail.com>
2024-12-28 21:28:23 +0100

    added radarr chart

--- cosas.sh
+++ cosas.sh
@@ -1,6 +1,7 @@
 helm dependency update prowlarr && \
 helm dependency update qbittorrent && \
 helm dependency update sonarr && \
+helm dependency update radarr && \
 helm install qbittorrent qbittorrent/ --debug --dry-run
 
 #helm dependency update basetemplate && \
