c7cfbcfa5244358b5f428df3387c44ab139722dc
oriol <filter.oriol@gmail.com>
2024-12-28 22:30:23 +0100

    added bazarr chart

--- cosas.sh
+++ cosas.sh
@@ -2,6 +2,7 @@ helm dependency update prowlarr && \
 helm dependency update qbittorrent && \
 helm dependency update sonarr && \
 helm dependency update radarr && \
+helm dependency update bazarr && \
 helm install qbittorrent qbittorrent/ --debug --dry-run
 
 #helm dependency update basetemplate && \
